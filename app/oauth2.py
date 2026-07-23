e1004496d5065997b126ac84d9c50fd8ce972781
LeonardoChavez777 <leonardodavid.chavez@hotmail.com>
2026-07-23 00:53:40 -0500

    now you really can login

--- app/oauth2.py
+++ app/oauth2.py
@@ -13,7 +13,7 @@ SECRET_KEY = settings.secret_key
 ALGORITHM = settings.algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
