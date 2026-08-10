188870dce6494e7637399ceda799c280b9b746cf
LeonardoChavez777 <leonardodavid.chavez@hotmail.com>
2026-08-10 18:54:18 -0500

    CICD completed I hope

--- app/main.py
+++ app/main.py
@@ -31,4 +31,4 @@ app.include_router(audio.router)
 
 @app.get("/")
 def root():
-    return {"message": "epico"}   
+    return {"message": "actualizado automaticamente gracias a github actions y docker compose"}   
