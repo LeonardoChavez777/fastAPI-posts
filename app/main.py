369b2f87d386d95660f4b2c73faedce904430816
LeonardoChavez777 <leonardodavid.chavez@hotmail.com>
2026-08-26 19:48:46 -0500

    bro ahora si pls

--- app/main.py
+++ app/main.py
@@ -31,4 +31,4 @@ app.include_router(audio.router)
 
 @app.get("/")
 def root():
-    return {"message": "lo mas epico actualizado en aws tambien :D"}
+    return {"message": "lo mas epico actualizado en aws tambien :D, tmr oe"}
